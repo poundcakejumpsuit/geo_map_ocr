23e5cffce847f19d98dbf0508c22f7cd63c445f9
Guest Account <guestaccount@Saras-MBP-8.attlocal.net>
2019-08-31 19:39:45 -0500

    first pass at parse_map file

--- lib/parse_map.py
+++ lib/parse_map.py
@@ -1,2 +1,19 @@
+import pytesseract
+from PIL import Image, ImageEnhance, ImageFilter
+
+
+MAPS_ROOT = '/Users/guestaccount/geo_map_ocr/maps/'
+
+
 def parse(mapfile):
-	print('hi!')
+	map = Image.open(mapfile)
+	filtered_map = map.filter(ImageFilter.MedianFilter())
+	enhancer = ImageEnhance.Contrast(filtered_map)
+	
+	filename = f'{mapfile.name.split('/')[-1].split('.')[0]}_prepared.pdf'
+	
+	preprocessed = enhancer.enhance(2).convert(1).save(MAPS_ROOT + 'out/' + filename)
+	
+	text = pytesseract.image_to_string(Image.open()
+	print(f'{mapfile.name} yielded: {text}')
+
